d395fc2b5537eca9a08b1230a36d1cf63f572690
Kabir060107 <24csu342@ncuindia.edu>
2025-02-16 13:45:28 +0530

    Q9.cpp

---
+++ Q9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows and columns for the matrix: ";
+    cin >> rows >> cols;
+    
+    int matrix[rows][cols], transpose[cols][rows];
+    cout << "Enter the matrix elements: ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    
+    cout << "Transpose of the matrix: " << endl;
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
